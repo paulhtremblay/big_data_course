cb86487257090138f7dfa3fa8f392239c7faa025
Paul Tremblay <paulhtremblay@gmail.com>
2018-07-14 09:42:29 -0700

    adding avg temp us

---
+++ python_code/spark_code/temp_us_states_rdd.py
@@ -0,0 +1,31 @@
+from pyspark import SparkContext, SparkConf
+conf = SparkConf().setAppName('Summer_Course').setMaster('local')
+sc = SparkContext(conf=conf)
+from parse_noaa import parse_line
+from stations_us_dict import d
+
+def add_state(the_dict):
+    state = d.get(str(the_dict['fixed_weather_station_usaf_master_station_catalog_identifier']), {}).get('st')
+    if state == '':
+        state = None
+    the_dict['us_state'] = state
+    return the_dict
+
+def get_avg(a, b):
+    return (a[0] + b[0], a[1] + b[1])
+
+rdd = sc.textFile('us_stations_90_sample_small.txt')\
+    .map(parse_line)\
+       .map(add_state)\
+       .filter(lambda x: x.get('us_state') != None)\
+       .filter(lambda x: x.get('air_temperature_observation_air_temperature') != None)\
+       .map(lambda x: (x.get('us_state'), (x.get('air_temperature_observation_air_temperature'),1)))\
+       .reduceByKey(get_avg)\
+       .map(lambda x: (x[0], x[1][0]/x[1][1]))\
+       .map(lambda x: '{us_state},{temp}'.format(us_state = x[0], temp = x[1]))\
+       .coalesce(1)\
+       .saveAsTextFile('states_results')
+
+
+
+
--- slides/lesson7_5.rst
+++ slides/lesson7_5.rst
@@ -4,30 +4,22 @@
 Outputing Contents of an RDD
 =========================================
 
+Let's look at :ref:`lesson3_4` again, and get the same results using an RDD in Spark.
 
-- Inefficient and often impossible to collect data.
 
-Look at lesson 3: :ref:`lesson3_3` 
-
-- Spark outputs an RDD to *multiple* files, to a directory
-- You can control the number of files with `coalesce`
-- We will cover more methods of writing when we cover dataframes
-
-.. literalinclude:: ../python_code/spark_code/write_rdd.py
+.. literalinclude:: ../python_code/map_reduce/reduce_state_temps.py
    :language: python
    :linenos:
-
 
 
 Questions
 =========
 
-1. Why should you generally avoid using `collect`?
+1. How many times is the data read and written from disk? How does that differ from the map/reduce job?
 
 Class Exercise
 ==============
 
-Read in the two texts from Dickens. Count the words. Save the file to Hadoop.
 
 
-<< :ref:`lesson7_4` | :ref:`lesson7_6`  >>
+<< :ref:`lesson7_5` | :ref:`lesson7_7`  >>
