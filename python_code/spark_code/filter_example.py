7e25bf3b921e1ef09b34fc04d747a5b0f20d26a4
Paul Tremblay <paulhtremblay@gmail.com>
2018-07-26 00:59:55 -0700

    fixing spark code

--- python_code/spark_code/filter_example.py
+++ python_code/spark_code/filter_example.py
@@ -1,8 +1,10 @@
+import os
 from pyspark import SparkContext, SparkConf
 conf = SparkConf().setAppName('Summer_Course').setMaster('local')
 sc = SparkContext(conf=conf)
-rdd1 = sc.textFile('simple_word_count_words.txt')
-rdd2 = rdd1.filter(lambda x: 'Car' in x)
+path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'simple_words_count.txt')
+rdd1 = sc.textFile('file:///{path}'.format(path = path))
+rdd2 = rdd1.filter(lambda x: 'car' in x)
 print(rdd2.collect())
 
 
